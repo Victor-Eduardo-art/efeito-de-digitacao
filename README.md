a0e3f839f221b4c036483af7696265956dae809a
victor <vcladara@gmail.com>
2022-03-05 13:36:51 -0300

    primeiro commit!

---
+++ README.md
@@ -0,0 +1,3 @@
+# EFEITO-DE-DIGITAção
+
+criei um efeito de digitação com delay com HTML CSS e JavaScript puro, é um código bem simples só pra praticar
---
+++ script.js
@@ -0,0 +1,55 @@
+let chave = false
+
+function iniciar () {
+   gerenciarTexto()
+}
+
+function gerenciarTexto () {
+   // essa função vai chamar a função efeitoDit
+   // a função efeitoDit recebe: texto, delay, classe, elementoPai, tmpExec
+   const aside = document.querySelectorAll("aside")[0]
+
+   efeitoDit("teste", 100, null, aside, 1)
+   calcTmp("ola mundo", 100, "colorRed", aside, 1000)
+   calcTmp("Hello World", 500, null, aside, 1)
+}
+
+function efeitoDit (texto, delay, classe, ElementoPai, tmpExec) {  
+// essa função é que vai criar o efeito de digitação
+
+   chave = true
+   const span = document.createElement("span")
+   const br = document.createElement("br")
+   const arrayTexto = texto.split("")
+
+   span.classList.add(classe)
+
+   ElementoPai.appendChild(span)
+   ElementoPai.appendChild(br)
+
+   let tmp = setTimeout(() => {
+      let pos = 0
+      let tmp2 = setInterval(() => {
+         if (span.innerHTML.length != arrayTexto.length) {
+            span.innerHTML = span.innerHTML + arrayTexto[pos]
+            pos++
+         }else {
+            span.classList.add("efeitoDit")
+            clearInterval(tmp2)
+            chave = false
+         }
+      }, delay)
+   }, tmpExec)
+}
+
+function calcTmp (texto, delay, classe, ElementoPai, tmpExec) {
+   const tmp = setInterval(() => {
+      console.log(chave)
+      if (chave === false) {
+         efeitoDit(texto, delay, classe, ElementoPai, tmpExec)
+         clearInterval(tmp)
+      }
+   }, 100)
+}
+
+window.addEventListener("load", iniciar)
